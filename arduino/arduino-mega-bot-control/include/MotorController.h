35cc59795106005460aae31f4b9b05f88d8a7789
navneet0401 <navneet.kaur@iitgn.ac.in>
2021-06-27 01:40:07 +0530

    Added doxygen in Motor Controller example

--- arduino/arduino-mega-bot-control/include/MotorController.h
+++ arduino/arduino-mega-bot-control/include/MotorController.h
@@ -1,50 +1,84 @@
-/*
-    This library defines a class to control the
-    motor giving the PWM value and its direction.
-*/
+/**
+ * @file MotorController.h
+ * @author Navneet Kaur (navneet.kaur@iitgn.ac.in)
+ * @brief This library defines a class to control the
+ * motor giving the PWM value and its direction.
+ * @version 0.1
+ * @date 2021-06-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #ifndef __MOTOR_CONTROLLER__
 #define __MOTOR_CONTROLLER__
 
-// This library contains the definition of functions
-// used for updating and extracting the value of angular
-// velocity. 
+/**
+ * @brief This library contains the definition of functions 
+ * used for updating and extracting the value of angular velocity. 
+ */
 #include "AngularVelocityCalculator.h"
 
-// This library contains a class to implement Proportional
-// Integra, and Differential control (PID).
+/**
+ * @brief This library contains a class to implement Proportional
+ * Integral, and Differential control (PID).
+ */
 #include "PID.h"
 
-// Class to update PWM values onto Motor-connected Arduino
-// pin.
+/**
+ * @brief Class to update PWM values onto Motor-connected 
+ * Arduino pin.
+ * 
+ */
 class MotorController : public AngularVelocityCalculator, public PID
 {
     private:
 
-        // Arduino pins for PWM value and direction
+        /**
+         * @brief Arduino pins for PWM value and direction
+         * 
+         */
         const uint8_t PWM_pin_;
         const uint8_t direction_pin_;
 
-        // Variable for updated angular velocity 
+        /**
+         * @brief Variable for updated angular velocity 
+         * 
+         */
         float angular_velocity_;
+        
+        /**
+         * @brief Variable for controller output 
+         * 
+         */
         float pid_output_;
-        // Vaariable for controller output 
 
     public :
 
-        // Update the PWM values 
+        /**
+         * @brief Update the PWM values 
+         * 
+         */
         inline void spinMotor() __attribute__((always_inline));
 
         float angVel();
         float pidOut();
 
-        // Constructor
-        // PWMpin - Pin no. on which absolute value of PWM is to be written
-        // directionPin - Pin no. on which sign (HIGH or LOW) of PWM value is to be written
-        // encoderPin1 - Pin no. to which Pin A of encoder is connected
-        // encoderPin2 - Pin no. to which Pin B of encoder is connected
-        // updateFrequency - Frequency at which new position value is read from the encoder
-        // countsPerRotation - No. of quadrature turn counts of encoder to complete one shaft rotation
+        /**
+         * @brief Construct a new Motor Controller object
+         * @param PWM_pin Pin no. on which absolute value of PWM is to 
+         * be written
+         * @param direction_pin Pin no. on which sign (HIGH or LOW) of 
+         * PWM value is to be written
+         * @param encoder_pin_1 Pin no. to which Pin A of encoder is 
+         * connected
+         * @param encoder_pin_2 Pin no. to which Pin B of encoder is 
+         * connected
+         * @param update_frequency Frequency at which new position 
+         * value is read from the encoder
+         * @param counts_per_rotation No. of quadrature turn counts of 
+         * encoder to complete one shaft rotation
+         */
         MotorController(
             uint8_t PWM_pin,
             uint8_t direction_pin,
@@ -56,23 +90,32 @@ class MotorController : public AngularVelocityCalculator, public PID
 
 };
 
-// Update and extract the angular velocity and calculate the pid
-// control values for the same. 
-// Write the PWM value and direction
+/**
+ * @brief Update and extract the angular velocity and calculate the pid
+ * control values for the same. Write the PWM value and direction
+ */
 void MotorController::spinMotor()
 {
-    // Update the angular velocity as per the new 
-    // captured position of the encoder.
+    /**
+     * @brief Construct a new update Angular Velocity object
+     * Update the angular velocity as per the new captured position of the encoder.
+    
+     */
     updateAngularVelocity();
     getAngularVelocity(angular_velocity_);
     
     pid_output_ = round(getControllerOutput(angular_velocity_));
 
-    // Write a HIGH or LOW value to the direction pin
+    /**
+     * @brief Write a HIGH or LOW value to the direction pin
+     * 
+     */
     digitalWrite(direction_pin_, pid_output_>0?HIGH:LOW);
 
-    // Write the absolute value of PWM value on the PWM
-    // pin.
+    /**
+     * @brief Write the absolute value of PWM value on the PWM pin
+     * 
+     */
     analogWrite(PWM_pin_, min(abs(pid_output_),0xFF));
 }
 
@@ -86,7 +129,16 @@ float MotorController::pidOut()
     return pid_output_;
 }
 
-// Constructor
+/**
+ * @brief Construct a new Motor Controller:: Motor Controller object
+ * 
+ * @param PWM_pin 
+ * @param direction_pin 
+ * @param encoder_pin_1 
+ * @param encoder_pin_2 
+ * @param update_frequency 
+ * @param counts_per_rotation 
+ */
 MotorController::MotorController(
     uint8_t PWM_pin,
     uint8_t direction_pin,
@@ -94,8 +146,10 @@ MotorController::MotorController(
     uint8_t encoder_pin_2,
     float update_frequency,
     float counts_per_rotation
-) : // Mem initialization list
-    // Calling the constructor of base classes
+) : /**
+ * @brief Construct a new Angular Velocity Calculator object
+ * Mem initialization list. Calling the constructor of base classes
+ */
 AngularVelocityCalculator(
     encoder_pin_1,
     encoder_pin_2,
@@ -103,11 +157,17 @@ AngularVelocityCalculator(
     counts_per_rotation
 ), 
 PID(update_frequency), 
-// Initialize constant variables
+/**
+ * @brief Initialize constant variables
+ * 
+ */
 PWM_pin_(PWM_pin),
 direction_pin_(direction_pin)
 {   
-    // Initilize all the private variables to zero
+    /**
+     * @brief Initilize all the private variables to zero
+     * 
+     */
     angular_velocity_ = 0;
     pid_output_ = 0;
 }
--- arduino/arduino-mega-bot-control/test/SpinMotorExample.cpp
+++ arduino/arduino-mega-bot-control/test/SpinMotorExample.cpp
@@ -1,11 +1,15 @@
-/*
-  This is an example for running the PID controls 
-  for a target velocity.
-
-  The angular velocity is updated at a rate of 50Hz.
-  PID output and angular velocity is printed at every 
-  2000 ms rate.
-*/
+/**
+ * @file SpinMotorExample.cpp
+ * @author Navneet Kaur (navneet.kaur@iitgn.ac.in)
+ * @brief Example for running PID controls for a target velocity.
+ * The angular velocity is updated at a rate of 50Hz.  PID output
+ * and angular velocity is printed at every 2000 ms rate.
+ * @version 0.1
+ * @date 2021-06-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include "MotorController.h"
 
@@ -14,15 +18,26 @@
 #define ENCODER_PIN_A 18
 #define ENCODER_PIN_B 20
 
-// Frequency at which velocity is updated
+/**
+ * @brief Frequency at which velocity is updated
+ * 
+ */
 #define VELOCITY_UPDATE_FREQUENCY 50 // Hz
+
 #define ENCODER_COUNTS_PER_ROTATION 840
 
-// Time interval at which anything is printed
-// to serial monitor.
+/**
+ * @brief Time interval at which anything is printed to serial monitor.
+
+ * 
+ */
 #define PRINT_TIME_PERIOD 2000 // ms
 
-// Our object for motor controller
+/**
+ * @brief Our object for motor controller
+ * 
+ * @return MotorController 
+ */
 MotorController motor_controller(
   PWM_PIN,
   DIRECTION_PIN,
@@ -32,29 +47,43 @@ MotorController motor_controller(
   ENCODER_COUNTS_PER_ROTATION
 );
 
-// Variable to store the last time anything was
-// printed through the serial port in millisecond.
+/**
+ * @brief Variable to store the last time anything was
+ * printed through the serial port in millisecond.
+ * 
+ */
 long int last_print_time;
+
+/**
+ * @brief Variable to store the last time the angular 
+ * velocity was updated in millisecond.
+ * 
+ */
 long int last_vel_update_time;
-// Variable to store the last time the angular 
-// velocity was updated in millisecond.
 
 void initialize_timer_2();
 
 void setup()
 {
-  // Initialise Serial Comm
+  /**
+   * @brief Initialise Serial Comm
+   * 
+   */
   Serial.begin(9600);
-
-  // initialize_timer_2();
 
   last_vel_update_time = millis();
   last_print_time = millis();
 
-  // Set PID gains to P=15, I=120 and D=0 
+  /**
+   * @brief Set PID gains to P=15, I=120 and D=0 
+   * 
+   */
   motor_controller.setPIDGains(15, 120, 0);
 
-  // Set target angular velocity
+  /**
+   * @brief Set target angular velocity
+   * 
+   */
   motor_controller.setTargetStateValue(10);
 
   Serial.println("Arduino Initialized successfully");
@@ -82,44 +111,4 @@ void loop()
 
     last_print_time = millis();
   }
-}
-
-void initialize_timer_2()
-{
-  // stop interrupts
-  cli();
-  
-  // Set timer2 to interrupt at 8kHz
-
-  // ATmega2560 clock frequency - 16MHz
-  // Prescalers available for timers - 1, 8, 64, 256, 1024
-  // Timer clock frequency = ATmega2560 clock frequency / Prescaler
-  
-  // To have interrupts with better frequency resolution timer
-  // will be operated in Clear Timer on Compare Match (CTC) mode
-  // TCNTx will count from 0x00 to the value in OCRnA register
-  // Frequency of Interrupt = Timer clock frequency / Number of TCNTn Counts before it resets to 0x00
-
-  // TCNTx will be compared to OCRnx in each clock cycle
-  // Upon compare match, interrupt is fired
-  // For 1kHz, TCNTx needs - 
-  //   - 250 counts at 64 prescaler
-  
-  // Turn on CTC mode
-  TCCR2A |= (0x01 << WGM21);
-  // Set Prescaler to 64
-  TCCR2B |= (0x01 << CS22);
-  TCCR2B |= (0x01 << CS20);
-  // Set compare match register (OCR2A) to 249
-  OCR2A = 0xF9;
-  // Enable interrupt upon compare match of OCR2A
-  TIMSK2 |= (0x01 << OCIE2A);
-
-  // allow interrupts
-  sei();
-}
-
-ISR(TIMER2_COMPA_vect)
-{
-  motor_controller.spinMotor();
 }
